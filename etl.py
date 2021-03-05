f6e8aaebacd5c19e5568001316c0236c21dc1480
Avais <avaisahmed03@gmail.com>
2021-03-05 14:04:34 +0000

    updated

--- etl.py
+++ etl.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 def extract(file_name):
-    raw_data = pd.read_csv(file_name, headers=["datetime", "location", "name", "item", "payment_method", "total_price", "card_details"]) #reads csv file and assigns given headers
+    raw_data = pd.read_csv(file_name, names=["datetime", "location", "name", "item", "payment_method", "total_price", "card_details"]) #reads csv file and assigns given headers
     df = pd.DataFrame(raw_data) #converts the csv into into a dataframe
     df.dropna(inplace = True) #Removes null values
-    df.drop_duplicates() #Removes duplicates 
+    df.drop_duplicates() #Removes duplicates
     return df
+
 
 def drop_sensitive_data(df):
     df.drop(df.columns[2], axis = 1, inplace = True) #Removes cust name 
