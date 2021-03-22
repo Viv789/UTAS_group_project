30323f03291a0ff824cbe18b034a84d1519426d9
WhatTheFoz <foziariazz12@gmail.com>
2021-03-22 17:17:48 +0000

    updated sql tables

--- src/create_tables.py
+++ src/create_tables.py
@@ -14,36 +14,33 @@ Ensure that you have created a cafe_data database first, then run this
 # create_tables
 
 def create_tables():
-    engine.execute('CREATE TABLE product ('
-                'product_id VARCHAR(255) NOT NULL,'
-                'product_name VARCHAR(255) NOT NULL,'
-                'size VARCHAR(255),'
-                'price FLOAT,'
-                'PRIMARY KEY (product_id))'
+    engine.execute(CREATE TABLE IF NOT EXISTS product (
+                product_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                product_name VARCHAR(255) NOT NULL,
+                size VARCHAR(255),
+                price FLOAT
             );
 
-    engine.execute('CREATE TABLE location ('
-                'location_id VARCHAR(255) NOT NULL,'
-                'location_name VARCHAR(255),'
-                'PRIMARY KEY (location_id))'
+    engine.execute(CREATE TABLE IF NOT EXISTS location (
+                location_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                location_name VARCHAR(255)
             );
 
-    engine.execute('CREATE TABLE transaction ('
-                'transaction_id VARCHAR(255) NOT NULL,'
-                'datetime  VARCHAR(255) NOT NULL,'
-                'location_id  VARCHAR(255) NOT NULL,'
-                'payment_method VARCHAR(255) NOT NULL,'
-                'order_total FLOAT,'
-                'PRIMARY KEY (transaction_id),'
-                'FOREIGN KEY (location_id) REFERENCES location (location_id))'
+    engine.execute(CREATE TABLE IF NOT EXISTS transaction (
+                transaction_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                datetime  VARCHAR(255) NOT NULL,
+                location_id  VARCHAR(255) NOT NULL,
+                payment_method VARCHAR(255) NOT NULL,
+                order_total FLOAT,
+                FOREIGN KEY (location_id) REFERENCES location (location_id))
             );
 
-    engine.execute('CREATE TABLE basket ('
-                'transaction_id VARCHAR(255) NOT NULL,'
-                'product_id  VARCHAR(255) NOT NULL,'
-                'CONSTRAINT fk_basket'
-                    'FOREIGN KEY (transaction_id) REFERENCES transaction (transaction_id) ON DELETE SET NULL,'
-                    'FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE SET NULL)'
+    engine.execute(CREATE TABLE IF NOT EXISTS basket (
+                transaction_id VARCHAR(255) NOT NULL,
+                product_id  VARCHAR(255) NOT NULL,
+                CONSTRAINT fk_basket
+                FOREIGN KEY (transaction_id) REFERENCES transaction (transaction_id),
+                FOREIGN KEY (product_id) REFERENCES product (product_id)
             );
 
 # To check if database is empty
