66b3c60cee8e02fb086a675fc9fa390f15aa37b0
John <jcwrightson@gmail.com>
2021-03-25 16:53:20 +0000

    fix

--- src/load.py
+++ src/load.py
@@ -6,20 +6,21 @@ from uuid import uuid4
 
 # load products table
 def load_products(row, conn):
+    cursor = conn.cursor()
     basket = row['basket']
     prod_ids = []
     for i, product in enumerate(basket):
         # ToDo: Check if exists first
         sql_query = "SELECT product_id FROM product WHERE product_name = %s AND size = %s"
         p_values = (str(product["name"]), str(product["size"]))
-        conn.cursor().execute(sql_query, p_values)
-        result = conn.cursor().fetchone()
+        cursor.execute(sql_query, p_values)
+        result = cursor.fetchone()
         if result == None:
             sql_query = "INSERT INTO product (product_id, product_name, size, price) VALUES (%s, %s, %s, %s)"
             prod_ids.append(str(uuid4()))
             vals = list(product.values())
             vals.insert(0, prod_ids[i])
-            conn.cursor().execute(sql_query, tuple(vals))
+            cursor.execute(sql_query, tuple(vals))
             conn.commit()
         else:
             prod_ids.append(result[0])
@@ -31,19 +32,21 @@ def load_products(row, conn):
 def load_location(row, conn):
     name = row['location']['name']
     loc_id = uuid4()
+    cursor = conn.cursor()
 
     # ToDo: Check if exists first - DONE
     sql_query = "SELECT location_id FROM location WHERE location_name = %s"
     l_values = str(name)
     try:
-        conn.cursor().execute(sql_query, (l_values,))
+        cursor.execute(sql_query, (l_values,))
     except Exception:
         conn.rollback()
-    result = conn.cursor().fetchone()[0]
+
+    result = cursor.fetchone()
     if result == None:
         sql_query = "INSERT INTO location (location_id, location_name) VALUES (%s, %s)"
         value = (str(loc_id), name)
-        conn.cursor().execute(sql_query, value)
+        cursor.execute(sql_query, value)
         conn.commit()
         return loc_id
     else:
@@ -53,14 +56,15 @@ def load_location(row, conn):
 
 # load transactions table
 def load_transaction(row, loc_id, conn):
+    cursor = conn.cursor()
     transaction = row['transaction']
     trans_id = uuid4()
-    timestamp = transaction['timestamp']
+    timestamp = transaction['date_time']
     pay_method = transaction['payment_method']
     order_total = transaction["total"]
     sql_query = "INSERT INTO transaction (transaction_id, date_time, location_id, payment_method, order_total) VALUES (%s, %s, %s, %s, %s)"
     values = (str(trans_id), timestamp, str(loc_id), pay_method, order_total)
-    conn.cursor().execute(sql_query, values)
+    cursor.execute(sql_query, values)
     conn.commit()
     return trans_id
 
@@ -68,11 +72,12 @@ def load_transaction(row, loc_id, conn):
 
 # load basket table
 def load_basket(row, trans_id, conn):
+    cursor = conn.cursor()
     prod_ids = load_products(row, conn)
     for p_id in prod_ids:
         sql_query = "INSERT INTO basket (transaction_id, product_id) VALUES (%s, %s)"
         b_values = (str(trans_id), str(p_id))
-        conn.cursor().execute(sql_query, b_values)
+        cursor.execute(sql_query, b_values)
         conn.commit()
 
     print("data loaded to basket table")
